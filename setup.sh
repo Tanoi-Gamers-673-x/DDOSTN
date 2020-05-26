c5456eccb82c53e2a9c4deb4537abd8a4698829f
Tanoi-Gamers-673-x <62834829+Tanoi-Gamers-673-x@users.noreply.github.com>
2020-05-26 13:34:21 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin
+clear
+pkg install python
+pkg install python2
+pkg install nano
+pkg install tor
+pkg install bash
+pkg install ruby
+pkg install pip
+pip install --upgrade pip
+pkg install npm
+gem install lolcat
+chmod +x ddos
+mv ddos /data/data/com.termux/files/usr/bin
